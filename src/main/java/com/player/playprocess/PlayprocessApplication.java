b8d9d7b1b621da368d296d94b94560f4d6d0818e
Kaja <kajaspiro@gmail.com>
2020-04-25 12:50:52 +0200

    initial commit

---
+++ src/main/java/com/player/playprocess/PlayprocessApplication.java
@@ -0,0 +1,13 @@
+package com.player.playprocess;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlayprocessApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlayprocessApplication.class, args);
+    }
+
+}

7c3d7bf7eef21680805e23bcdc96331fd257f783
Kaja <kajaspiro@gmail.com>
2020-05-03 16:14:35 +0200

    Use slider to slide movie, run from selected part

--- src/main/java/com/player/playprocess/controller/VideoController.java
+++ src/main/java/com/player/playprocess/controller/VideoController.java
@@ -3,12 +3,12 @@ package com.player.playprocess.controller;
 import javafx.beans.binding.Bindings;
 import javafx.beans.property.DoubleProperty;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Button;
 import javafx.scene.control.Slider;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 import javafx.scene.media.MediaView;
+import javafx.util.Duration;
 import org.springframework.stereotype.Controller;
 
 import java.net.URL;
@@ -40,6 +40,7 @@ public class VideoController {
 
     public void runMedia() {
         URL resource = UIController.class.getResource("/video/VID_20191004_211955.mp4");//todo select file from computer
+
         this.mediaPlayer = new MediaPlayer(new Media(resource.toExternalForm()));
         this.video.setMediaPlayer(this.mediaPlayer);
         DoubleProperty height = this.video.fitHeightProperty();
@@ -47,19 +48,34 @@ public class VideoController {
         width.bind(Bindings.selectDouble(this.video.sceneProperty(), "width"));
         height.bind(Bindings.selectDouble(this.video.sceneProperty(), "height"));
 
+        this.mediaPlayer.setOnReady(() -> {
+            this.videoSlider.setMax(this.mediaPlayer.getTotalDuration().toSeconds());
+        });
+
+        //works
+        this.videoSlider.valueProperty().addListener((obs, oldV, newV) -> {
+            Duration videoDuration = this.mediaPlayer.getMedia().getDuration();
+            double sliderValue = this.videoSlider.getValue();
+            if (this.videoSlider.isValueChanging()) {
+                Duration test = videoDuration.multiply(sliderValue / 10.0);
+                this.mediaPlayer.seek(test);
+            }
+        });
+
         this.mediaPlayer.setOnEndOfMedia(() -> {
             this.videoSlider.setValue(0.0);
         });
 
         this.mediaPlayer.currentTimeProperty().addListener((observableValue, oldTime, newTime) -> {
             System.out.println(newTime);
-            this.videoSlider.setValue(mediaPlayer.getCurrentTime().toSeconds());
+            if (mediaPlayer.getStatus() == MediaPlayer.Status.PLAYING) {
+                this.videoSlider.setValue(mediaPlayer.getCurrentTime().toSeconds());
+            }
         });
     }
 
     @FXML
-    public void playVideo() {//todo do it in better way
-        this.videoSlider.setMax(this.mediaPlayer.getTotalDuration().toSeconds());
+    public void playVideo() {
         this.mediaPlayer.play();
     }
 
